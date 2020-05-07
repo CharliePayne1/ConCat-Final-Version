a058ff0784c23ae34cdf1049b060b9800d814b74
CharliePayne1 <charliepayne1101@gmail.com>
2020-05-07 07:51:13 +0100

    updated presentation on active projects

--- bin/run.rb
+++ bin/run.rb
@@ -23,7 +23,8 @@ CLI.loading_bar
             if project_names.length == 0
             puts "You don't have any active projects, let's change that!.".colorize(:red)
             else
-            puts "Here are all the names of your active projects: #{project_names.to_s}.".colorize(:green)
+                string = project_names.join(", ")
+            puts "Here are all the names of your active projects: #{string}.".colorize(:green)
             end
     else
     puts "That means you must have a new App idea!".colorize(:yellow)
@@ -31,56 +32,72 @@ end
 
 #input project language (READ)
 puts " "
-new_app_language = prompt.select("Thanks for choosing ConCat to create your new app #{new_user_name}, now let us know which program you want your new app built in:", %w(PPL es Oz Rlab HyperTalk Unicon Harbour FlooP TypeScript TEX Golo NWScript Hermes SystemVerilog Ratfor M# PL/SQL))
-developer = Developer.all.find {|developer| developer.language == new_app_language}
-CLI.loading_bar
+new_project = prompt.yes?("Would you like to create a new app?".colorize(:yellow))
+if new_project
+    CLI.loading_bar 
+    new_app_language = prompt.select("Thanks for choosing ConCat to create your new app #{new_user_name}, now let us know which program you want your new app built in:", %w(PPL es Oz Rlab HyperTalk Unicon Harbour FlooP TypeScript TEX Golo NWScript Hermes SystemVerilog Ratfor M# PL/SQL))
+    developer = Developer.all.find {|developer| developer.language == new_app_language}
+    CLI.loading_bar
 
-#show the user their match 
-puts "Congratulations, we've matched you with #{developer.name}, who is an expert in #{new_app_language}.".colorize(:green)
-puts " "
+    #show the user their match 
+    puts "Congratulations, we've matched you with #{developer.name}, who is an expert in #{new_app_language}.".colorize(:green)
+    puts " " 
 
-#get the name of the new app & category to create a new project instance
-new_app_name = prompt.ask("If you'd like to start working with #{developer.name}, tell us the name of your new app:".colorize(:yellow))
-app_category = prompt.select("And the category this falls into:", %w(Banking Communicaton E-Commerce Fitness Gaming Healthcare Pharmaceuticals TOP-SECRET Travel))
-CLI.loading_bar
+    #get the name of the new app & category to create a new project instance
+    new_app_name = prompt.ask("If you'd like to start working with #{developer.name}, tell us the name of your new app:".colorize(:yellow))
+    app_category = prompt.select("And the category this falls into:", %w(Banking Communicaton E-Commerce Fitness Gaming Healthcare Pharmaceuticals TOP-SECRET Travel))
+    CLI.loading_bar
 
 #create a new project instance (CREATE)
-Project.create(name: "#{new_app_name}", category: "#{app_category}", developer_id: developer.id, user_id: new_user.id)
-puts "Okay great, we've created a new project for you and #{developer.name} to start working on #{new_app_name} together.".colorize(:green)
-puts " "
-puts "Drop them an email to say hi and get started on #{developer.email}.".colorize(:green)
-puts " "
+    Project.create(name: "#{new_app_name}", category: "#{app_category}", developer_id: developer.id, user_id: new_user.id)
+    puts "Okay great, we've created a new project for you and #{developer.name} to start working on #{new_app_name} together.".colorize(:green)
+    puts " "
+    puts "Drop them an email to say hi and get started on #{developer.email}.".colorize(:green)
+    puts " "
 
+else
+    puts " "
+    puts "Okay, we'll move on."
+    puts " "
+end
 
 #update the name of your project (UPDATE)
-update_answer = prompt.yes?('Would you like to change the name of your app?'.colorize(:yellow))
+update_answer = prompt.yes?('Would you like to change the name of any of your apps?'.colorize(:yellow))
         if update_answer
+            puts " "
             original_app_name = prompt.ask("To help us find your app, what did you call it originally?".colorize(:yellow))
             original_app = Project.find_by(name: "#{original_app_name}")
-
+            puts " "
+            CLI.loading_bar
             updated_app_name = prompt.ask('Found it! Now, what would you like to change the name to?'.colorize(:green))
             original_app.update(name: "#{updated_app_name}")
             CLI.loading_bar
 
-        puts "Great, we've changed your app to be called #{updated_app_name}.".colorize(:green)
+        puts "Nice choice, we've changed your app to be called #{updated_app_name}.".colorize(:green)
+        puts " "
 
         else
             CLI.loading_bar
-            puts "Nice, we liked #{new_app_name} too.".colorize(:green)
+            puts " "
+            puts "If it ain't broke, don't fix it, right!"
+            puts " "
         end
 
 #delete the new project (DELETE)
 delete_project = prompt.yes?('Would you like to cancel a project?')
 if delete_project
+    puts " "
     app_name = prompt.ask("To help us find your app, what did you call your it?".colorize(:yellow))
     app = Project.find_by(name: "#{app_name}")
     app.delete
     CLI.loading_bar
+    puts " "
     puts "Okay, we've deleted this project for you.".colorize(:green)
 
 else
     CLI.loading_bar
-    puts "Great, can't wait to see it when it's done.".colorize(:green)
+    puts " "
+    puts "Great, can't wait to see your work!.".colorize(:green)
 
 end
 
